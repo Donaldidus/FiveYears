5ea198ca7681ad2b58db3c7bac1565b5e5d82248
Jan B <Jan@jans-mbp>
2017-05-10 23:38:45 +0200

    Removed some unnecessary code.

--- FiveYears/Constants.swift
+++ FiveYears/Constants.swift
@@ -56,33 +56,3 @@ extension UIColor {
         self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
     }
 }
-
-extension UIBezierPath {
-    convenience init(heartIn rect: CGRect) {
-        self.init()
-        
-        //Calculate Radius of Arcs using Pythagoras
-        let sideOne = rect.width * 0.4
-        let sideTwo = rect.height * 0.3
-        let arcRadius = sqrt(sideOne*sideOne + sideTwo*sideTwo)/2
-        
-        //Left Hand Curve
-        self.addArc(withCenter: CGPoint(x: rect.width * 0.3, y: rect.height * 0.35), radius: arcRadius, startAngle: 135.degreesToRadians, endAngle: 315.degreesToRadians, clockwise: true)
-        
-        //Top Centre Dip
-        self.addLine(to: CGPoint(x: rect.width/2, y: rect.height * 0.2))
-        
-        //Right Hand Curve
-        self.addArc(withCenter: CGPoint(x: rect.width * 0.7, y: rect.height * 0.35), radius: arcRadius, startAngle: 225.degreesToRadians, endAngle: 45.degreesToRadians, clockwise: true)
-        
-        //Right Bottom Line
-        self.addLine(to: CGPoint(x: rect.width * 0.5, y: rect.height * 0.95))
-        
-        //Left Bottom Line
-        self.close()
-    }
-}
-
-extension Int {
-    var degreesToRadians: CGFloat { return CGFloat(self) * .pi / 180 }
-}
